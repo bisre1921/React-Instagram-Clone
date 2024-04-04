b8b4176f8fd57771b50231baf3bf823963970bf4
bisre1921 <bisrattewodros3@gmail.com>
2024-04-04 16:38:44 +0300

    handle input change for post page postFormData

--- src/Pages/Post.jsx
+++ src/Pages/Post.jsx
@@ -1,4 +1,30 @@
+import { useState } from "react";
+
 const Post  = () => {
+    const [postData , setPostData] = useState({
+        postImage : "" , 
+        postCaption : "" ,
+    });
+    const {postImage , postCaption} = postData;
+
+    const handlePostDataFormInputChange = (event) => {
+        if(event.target.files) {
+            setPostData((prevState) => ({
+                ...prevState , 
+                postImage : event.target.files
+            }))
+        } 
+        if(!event.target.files) {
+            setPostData((prevState) => ({
+                ...prevState , 
+                [event.target.id] : event.target.value
+            }))
+        }
+
+        console.log(postCaption);
+       
+       
+    }
     return (
         <div className="h-svh bg-black text-white px-4">
             <div className="flex justify-center items-center gap-6 mb-10">
@@ -13,11 +39,21 @@ const Post  = () => {
                 </h1>
             </div>
             <div className="flex justify-center items-center ">
-                <form action="">
+                <form >
                     <label htmlFor="" className="pb-4">Choose images to post</label> <br />
-                    <input type="file" className="w-full mb-4" /> <br />
+                    <input 
+                        type="file" 
+                        className="w-full mb-4" 
+                        onChange={handlePostDataFormInputChange}
+                    /> <br />
                     <label htmlFor="" className="pb-4">Add Caption</label> <br />
-                    <textarea type="text" className="w-full" /> <br />
+                    <textarea 
+                        type="text" 
+                        className="w-full text-black"
+                        id="postCaption" 
+                        value={postCaption}
+                        onChange={handlePostDataFormInputChange}
+                    /> <br />
                     <div className="flex items-start justify-center mt-4">
                         <button className="border-2 px-4 rounded">
                             Post
