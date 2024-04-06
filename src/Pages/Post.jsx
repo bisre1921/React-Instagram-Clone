1b7f5b05640d4e6cff6a1a2ef3fc76874ec5c9a6
bisre1921 <bisrattewodros3@gmail.com>
2024-04-06 10:13:30 +0300

    upload postData to firestore database and postImage to storage

--- src/Pages/Post.jsx
+++ src/Pages/Post.jsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import {v4 as uuidv4} from "uuid";
+import { FaRegAddressCard } from "react-icons/fa";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase";
+import {toast} from "react-toastify";
 
 const Post  = () => {
+    const auth = getAuth();
+
     const [postData , setPostData] = useState({
         postImage : "" , 
         postCaption : "" ,
@@ -20,48 +29,98 @@ const Post  = () => {
                 [event.target.id] : event.target.value
             }))
         }
-
-        console.log(postCaption);
-       
-       
     }
+
+
+    const handlePostDataFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const storePostImages = (image) => {
+                return new Promise((resolve , reject) => {
+                    const storage = getStorage();
+                    const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+                    const storageRef = ref(storage , fileName);
+                    const uploadTask = uploadBytesResumable(storageRef , image);
+
+                    uploadTask.on("state_changed" , (
+                        snapshot => {
+                            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        }, 
+                        (error) => {
+                            reject(error);
+                        },
+                        () => {
+                            getDownloadURL(uploadTask.snapshot.ref).then((downloadORL) => {
+                                resolve(downloadORL);
+                            })
+                        }
+                    ))
+                })
+            };
+
+            const imageUrls = await Promise.all(
+                [...postImage].map((image) => storePostImages(image))
+            ).catch((error) => {
+                toast.error("Post Images not uploaded");
+                return;
+            });
+
+            const postDataCopy = {
+                ...postData , 
+                imageUrls , 
+                timestamp : serverTimestamp() , 
+                userRef : auth.currentUser.uid
+            };
+            delete postDataCopy.postImage;
+
+            const docRef = await addDoc(collection(db , "Posts") , postDataCopy);
+            toast.success("Post submitted Successfully");
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
     return (
-        <div className="h-svh bg-black text-white px-4">
-            <div className="flex justify-center items-center gap-6 mb-10">
-                <h1 className="text-xl font-bold">
-                    POST
-                </h1>
-                <h1 className="text-xl">
-                    STORY
-                </h1>
-                <h1 className="text-xl">
-                    REEL
-                </h1>
+        <div className="bg-zinc-200">
+            <div className="h-svh max-w-2xl mx-auto bg-black text-white px-4">
+                <div className="flex justify-center items-center gap-6 mb-10">
+                    <h1 className="text-xl font-bold">
+                        POST
+                    </h1>
+                    <h1 className="text-xl">
+                        STORY
+                    </h1>
+                    <h1 className="text-xl">
+                        REEL
+                    </h1>
+                </div>
+                <div className="flex justify-center items-center ">
+                    <form onSubmit={handlePostDataFormSubmit}>
+                        <label htmlFor="" className="pb-4">Choose images to post</label> <br />
+                        <input 
+                            type="file" 
+                            className="w-full mb-4" 
+                            onChange={handlePostDataFormInputChange}
+                        /> <br />
+                        <label htmlFor="postCaption" className="pb-4">Add Caption</label> <br />
+                        <textarea 
+                            type="text" 
+                            className="w-full text-black"
+                            id="postCaption" 
+                            value={postCaption}
+                            onChange={handlePostDataFormInputChange}
+                        /> <br />
+                        <div className="flex items-start justify-center mt-4">
+                            <button className="border-2 px-4 rounded">
+                                Post
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div className="flex justify-center items-center ">
-                <form >
-                    <label htmlFor="" className="pb-4">Choose images to post</label> <br />
-                    <input 
-                        type="file" 
-                        className="w-full mb-4" 
-                        onChange={handlePostDataFormInputChange}
-                    /> <br />
-                    <label htmlFor="" className="pb-4">Add Caption</label> <br />
-                    <textarea 
-                        type="text" 
-                        className="w-full text-black"
-                        id="postCaption" 
-                        value={postCaption}
-                        onChange={handlePostDataFormInputChange}
-                    /> <br />
-                    <div className="flex items-start justify-center mt-4">
-                        <button className="border-2 px-4 rounded">
-                            Post
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
+           </div> 
     )
 };
 export default Post;
